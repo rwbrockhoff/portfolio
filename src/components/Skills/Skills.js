2d46c6d595962a55e717b620b2a17f0cd1a37199
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-10-28 17:29:54 -0500

    Postgres and Jest added.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -6,10 +6,11 @@ import CSS from '../../assets/icons/css.png'
 import JS from '../../assets/icons/js.jpg'
 import REACT from '../../assets/icons/react.png'
 import VUE from '../../assets/icons/vue.png'
-import EXP from '../../assets/icons/express.png'
-import FIREBASE from '../../assets/icons/firebase.png'
+import NODE from '../../assets/icons/node.png'
 import SOCKET from '../../assets/icons/socket.svg'
 import ADOBE from '../../assets/icons/adobe.png'
+import POSTGRES from '../../assets/icons/postgres.png'
+import JEST from '../../assets/icons/jest.png'
 
 export default () => {
   return (
@@ -21,8 +22,9 @@ export default () => {
     alt="JS"/><p>javascript</p></div>
     <div className="indivIcon wow fadeInUp" data-wow-delay="0.25s"><img className="wow fadeInUp" src={REACT} alt="React, React Native"/><p>react / react native</p></div>
     <div className="indivIcon wow fadeInUp" data-wow-delay="0.3s"><img className="wow fadeInUp" src={VUE} alt="Vue"/><p>vue</p></div>
-    <div className="indivIcon wow fadeInUp" data-wow-delay="0.35s"><img className="wow fadeInUp" src={EXP} alt="Express"/><p>express</p></div>
-    <div className="indivIcon wow fadeInUp" data-wow-delay="0.4s"><img className="wow fadeInUp" src={FIREBASE} alt="Firebase"/><p>firebase</p></div>
+    <div className="indivIcon wow fadeInUp" data-wow-delay="0.35s"><img className="wow fadeInUp" src={NODE} alt="Node"/><p>node</p></div>
+    <div className="indivIcon wow fadeInUp" data-wow-delay="0.5s"><img className="wow fadeInUp" src={POSTGRES}alt="Postgres"/><p>postgres</p></div>
+    <div className="indivIcon wow fadeInUp" data-wow-delay="0.5s"><img className="wow fadeInUp" src={JEST}alt="Jest"/><p>jest</p></div>
     <div className="indivIcon wow fadeInUp" data-wow-delay="0.45s"><img className="wow fadeInUp" src={SOCKET} alt="Socket.io"/><p>socket.io</p></div>
     <div className="indivIcon wow fadeInUp" data-wow-delay="0.5s"><img className="wow fadeInUp" src={ADOBE}alt="Adobe Creative Suite"/><p>adobe creative suite</p></div>
   </div>
